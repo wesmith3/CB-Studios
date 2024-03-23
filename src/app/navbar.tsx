596788cd7c609376d19fb3b7c83fc6d5c92db2cc
Wesley <wesmith314@gmail.com>
2024-03-22 22:54:35 -0700

    removes links

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,36 +6,24 @@ function NavBar() {
 
   return (
     <div className="navbar">
-      <Link to='/'>
         <Button className='menu-btn'>
           Home
         </Button>
-      </Link>
-      <Link to='/about'>
         <Button className='menu-btn'>
           About Us
         </Button>
-      </Link>
-      <Link to='/services'>
         <Button className='menu-btn'>
           Services
         </Button>
-      </Link>
-      <Link to='/portfolio'>
         <Button className='menu-btn'>
           Portfolio
         </Button>
-      </Link>
-      <Link to='/projects'>
         <Button className='menu-btn'>
           Projects
         </Button>
-      </Link>
-      <Link to='/contact'>
         <Button className='menu-btn'>
           Contact
         </Button>
-      </Link>
     </div>
   );
 }
