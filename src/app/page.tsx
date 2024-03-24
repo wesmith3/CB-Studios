ec3bc3e7312cc5ab59b5dd00d56fa7c879cfd5c8
Wesley <wesmith314@gmail.com>
2024-03-23 20:16:04 -0700

    adds home heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import NavBar from './navbar'
 
 function Home() {
   return (
+    <>
     <div className='home'>
       <NavBar />
+      <div>
+        <h1 className="heading">Siri Miri</h1>
+        <h4 className="subhead">Film By Cloud Based Studios</h4>
+      </div>
     </div>
+    </>
   )
 }
 
