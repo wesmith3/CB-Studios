82210e6813636f4c5e70b303e21084c4b2aa9938
Wesley <wesmith314@gmail.com>
2024-08-15 08:48:42 -0700

    changes header to transparent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ function Home() {
     <div className='home'>
       <NavBar />
       <div>
-        <h1 className="heading">Siri Miri</h1>
+        <h1 className="heading">Cloud Based Studios</h1>
         <h4 className="subhead">Film By Cloud Based Studios</h4>
       </div>
     </div>
