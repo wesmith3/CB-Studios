fe5f67aedbbeae25fd36b52d8a80d925ca4b1303
Wesley <wesmith314@gmail.com>
2024-08-21 07:16:04 -0700

    fixes background size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-"use client"
-import About from './about'
-import NavBar from './navbar'
+"use client";
+import About from "./about";
+import NavBar from "./navbar";
+import Plot from "./plot";
 
 function Home() {
   return (
     <>
-    <div className='home'>
-      <NavBar />
-      <div>
-        <h4 className="subhead">A Film By Cloud Based Studios</h4>
-        <h1 className="heading">DESPERADO</h1>
-        <h1 className="bg_heading">MASTERPIECE</h1>
+      <div className="home">
+        <NavBar />
+        <div>
+          <h4 className="subhead">Film By Wesley Smith</h4>
+          <h1 className="heading">DESPERADO</h1>
+          <h1 className="bg_heading">MASTERPIECE</h1>
+        </div>
       </div>
-    </div>
-      <About/>
+      <Plot />
+      <About />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/app/plot.tsx
+++ src/app/plot.tsx
@@ -1,10 +1,10 @@
-import 'plot.css'
+import './plot.css'
 
 function Plot() {
   return (
-    <div>
+    <div className='plot'>
       <h1 className='synopsis'>Synopsis</h1>
-      <p className='plot'>Lorem ipsum dolor sir amet, consecte tuer adipiscing elit. Aenean cou ado ligula eg dolor. Aenean massa. Cum sociis Theme natoque penar bus et magnis dis parturient montes, nascet ridiculus mus. Phaselus viverra mulla ut metus varius looreet. Quisque rutrum enean imperdict etiam ultricies nisi vel augue, Curabitur ullamcorper ultricies.
+      <p className='syn_description'>Lorem ipsum dolor sir amet, consecte tuer adipiscing elit. Aenean cou ado ligula eg dolor. Aenean massa. Cum sociis Theme natoque penar bus et magnis dis parturient montes, nascet ridiculus mus. Phaselus viverra mulla ut metus varius looreet. Quisque rutrum enean imperdict etiam ultricies nisi vel augue, Curabitur ullamcorper ultricies.
       </p>
     </div>
   )
